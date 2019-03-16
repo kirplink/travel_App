7f171759f2e917e1462569cf00889ee01b406a94
daniel 'kirplink' reynolds <=>
2019-03-15 21:39:12 -0500

    heroku test

--- server.js
+++ server.js
@@ -37,10 +37,6 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
 	//set static folder
 	app.use(express.static("client/build"));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dir, "client", "build", "index.html"));
-	});
 }
 
 // Start the API server
