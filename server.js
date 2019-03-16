d6ff8b14bad707616dc3270379824186a885fc09
daniel 'kirplink' reynolds <=>
2019-03-15 21:46:17 -0500

    hhh

--- server.js
+++ server.js
@@ -36,7 +36,10 @@ app.use(routes);
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 	//set static folder
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client/build")));
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, "/client/build/index"));
+	});
 }
 
 // Start the API server
